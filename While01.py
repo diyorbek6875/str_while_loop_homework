99e68cbc96ccd27854c0ce426aa17cbd7446f60a
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-24 11:38:02 +0500

    1

--- While01.py
+++ While01.py
@@ -6,4 +6,15 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    k=0
+    a=len(s)
+    while i<a:
+        if s[i].isdigit():
+            k=k+1
+
+        i=i+1
+
+    return k
+s=str(input())
+print(main(s))
