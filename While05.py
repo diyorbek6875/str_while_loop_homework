b8986ea5a3f46810f2567b031d992805a7d261c2
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-25 13:08:46 +0500

    5

--- While05.py
+++ While05.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    i=0
+    a=len(s)
+    b=0
+    while i<a:
+        if s[i].islower():
+            b=b+1
+        i=i+1
+
+    return b 
+s=input()
+print(main(s))
