21632f2ec2d1351f24af1ca2c0f78cb0cca4d449
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-25 13:35:11 +0500

    9

--- While09.py
+++ While09.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=str(s)
+    i=0
+    b=len(a)
+    sum=0
+    while i<b:
+        sum=sum+int(s[i])
+        i=i+1
+
+    return sum
+s=input()
+print(main(s))
