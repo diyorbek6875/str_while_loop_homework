0644c9fead93fe1d0d23ac2dc06b8747c2eb0d38
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-25 12:46:20 +0500

    2

--- While02.py
+++ While02.py
@@ -6,4 +6,13 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=len(s)
+    b=0
+    i=0
+    while i<a:
+        if s[i].isalpha():
+            b=b+1
+        i=i+1
+    return b
+s=input()
+print(main(s))
