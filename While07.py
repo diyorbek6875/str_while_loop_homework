4f754da469f62e92a76640bd5d4a6f385baaa10d
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-25 13:28:57 +0500

    7

--- While07.py
+++ While07.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=len(s)
+    b=0
+    i=0
+    while i<a:
+        if s[i]=="2" or s[i]=='4' or s[i]=='6' or s[i]=='8' or s[i]=='0':
+            b=b+1
+        i=i+1
+
+    return b
+s=input()
+print(main(s))
