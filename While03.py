c5c8b4ff3ee8f7e752fb76b4b28047cb35b47214
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-25 13:01:27 +0500

    3

--- While03.py
+++ While03.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a=len(s)
+    b=0
+    i=0
+    while i<a:
+        if s[i].isdigit() or s[i].isalpha() or s[i].isspace():
+            b=b
+
+        else:
+            b=b+1
+        i=i+1
+        
+    return b
+s=input()
+print(main(s))
